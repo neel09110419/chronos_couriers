d7993c929e5389ba42d38186e2bedce612faad7b
Neelanshi Rai <neelanshi.as@gmail.com>
2025-06-23 23:42:20 +0530

    Project Setup
    Basic folder structure initialized for development

---
+++ src/main/java/com/chronos/couriers/ChronosCouriersApplication.java
@@ -0,0 +1,13 @@
+package com.chronos.couriers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronosCouriersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronosCouriersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chronos/couriers/ChronosCouriersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chronos.couriers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronosCouriersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
